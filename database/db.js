35ee562eb7831031acb451e3a0b509a179e955ac
Maximiliano <maxirpc2607@gmail.com>
2021-11-25 17:30:03 -0300

    Update conexion a db

--- database/db.js
+++ database/db.js
@@ -2,8 +2,18 @@ const mysql = require("mysql");
 
 // conexion a DB obj config
 const conexion = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "crud_nodejs_db",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud_nodejs_db",
 });
+
+conexion.connect((error) => {
+  if (error) {
+    console.error("EL error de conexion es: " + error);
+    return;
+  }
+  console.log("Conectado a la BD Mysql");
+});
+
+module.exports = conexion;
--- router.js
+++ router.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 // crear routes
 const router = express.Router();
+// conexion a db
+const conexion = require("./database/db");
 
 router.get("/contacto", (req, res) => {
-	res.send("Contacto");
+  conexion.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 module.exports = router;
