00b38e3fab15b2e830e47bbcb85292f110d24e30
Maximiliano <maxirpc2607@gmail.com>
2021-11-26 17:19:51 -0300

    controlador y vistas
    Se crean controlador de update , se crean vistas edit mostrando datos y eliminar.

--- controllers/crud.js
+++ controllers/crud.js
@@ -4,10 +4,28 @@ exports.save = (req, res) => {
   // recibir datos
   const user = req.body.user;
   const rol = req.body.rol;
-  // query a db
+  // query a db insert
   conexion.query(
     "INSERT INTO users SET ?",
     { user: user, rol: rol },
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+};
+
+exports.update = (req, res) => {
+  const id = req.body.id;
+  const user = req.body.user;
+  const rol = req.body.rol;
+  // query a db update
+  conexion.query(
+    "UPDATE users SET? WHERE id = ?",
+    [{ user: user, rol: rol }, id],
     (error, results) => {
       if (error) {
         console.log(error);
